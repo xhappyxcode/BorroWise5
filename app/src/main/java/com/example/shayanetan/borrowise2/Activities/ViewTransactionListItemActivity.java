9530e68124c281fef6f6fce5b5ce28c1659e4b5f
xhappyxcode <stephanie_dy@dlsu.edu.ph>
2016-07-22 12:00:24 +0800

    Committed by Stephanie Dy on 7/22/2016 11:58 am

---
+++ app/src/main/java/com/example/shayanetan/borrowise2/Activities/ViewTransactionListItemActivity.java
@@ -0,0 +1,15 @@
+package com.example.shayanetan.borrowise2.Activities;
+
+import android.os.Bundle;
+
+import com.example.shayanetan.borrowise2.R;
+
+public class ViewTransactionListItemActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_transaction_list_item);
+    }
+
+}
